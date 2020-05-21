c3da964ef0db71a43560775e07347147b59e300c
Chen Lijun <chenlijun1999@gmail.com>
2020-05-21 17:04:20 +0200

    build: fix and set up CMake for p and q

---
+++ src/analyzer/q/q_self.c
@@ -0,0 +1,13 @@
+#include "../q/q.h"
+#include "q.h"
+
+#define LOG_TAG "q"
+#include "../../logger.h"
+
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int unitnos_q_self_main(int in_pipe, int output_pipe) {
+  return 0;
+}
