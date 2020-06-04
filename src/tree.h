0bc965d39f9002522e372943f60e90eaeea07091
Chen Lijun <chenlijun1999@gmail.com>
2020-06-04 21:07:24 +0200

    minor(tree): normalize API comment sections

--- src/tree.h
+++ src/tree.h
@@ -23,42 +23,47 @@ typedef struct unitnos_node {
 typedef struct unitnos_tree {
   unitnos_node *root;
   int size;
-
-  // compare deve restituire 1 se v2 è maggiore, 0 se uguale o -1 se è minore
+  /**
+   * compare deve restituire 1 se v2 è maggiore, 0 se uguale o -1 se è minore
+   */
   int (*compare)(void *v1, void *v2);
-  // ciascun tipo ha una funzione propria per la liberazione della memoria
-  // deve restituire 1 se va a buon fine, 0 altrimenti
+  /**
+   * ciascun tipo ha una funzione propria per la liberazione della memoria deve
+   * restituire 1 se va a buon fine, 0 altrimenti
+   */
   int (*free_mem)(void *value);
 } unitnos_tree;
 
-/*
-        restituisce un nuovo albero
-        ciascun albero ha le sue funzioni per il confronto e la liberazione
-   della memoria
-*/
+/**
+ * Restituisce un nuovo albero
+ *
+ * Ciascun albero ha le sue funzioni per il confronto e la liberazione della
+ * memoria
+ */
 unitnos_tree *unitnos_tree_create(int (*compare)(void *v1, void *v2),
                                   int (*free_mem)(void *value));
 
-/*
-        aggiunge un nodo con il valore indicato
-*/
+/**
+ * Aggiunge un nodo con il valore indicato
+ */
 void unitnos_tree_add_node(unitnos_tree *tree, void *value);
 
-/*
-        restituisce un nodo col valore indicato
-        NULL se non lo trova
-*/
+/**
+ * Restituisce un nodo col valore indicato
+ *
+ * \retval NULL se non lo trova
+ */
 unitnos_node *unitnos_tree_search(unitnos_tree *tree, void *value);
 
-/*
-        rimuove il nodo che coincide col valore indicato
-*/
+/**
+ * Rimuove il nodo che coincide col valore indicato
+ */
 void unitnos_tree_remove(unitnos_tree *tree, void *value);
 
-/*
-        rimuovono ricorsivamente tutti i nodi
-        a partire da un nodo specificato [destroy] o dalla root [destroy_all]
-*/
+/**
+ * Rimuovono ricorsivamente tutti i nodi a partire da un nodo specificato
+ * [destroy] o dalla root [destroy_all]
+ */
 void unitnos_tree_destroy(unitnos_tree *tree, unitnos_node *node);
 void unitnos_tree_destroy_all(unitnos_tree *tree);
 
