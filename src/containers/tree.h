aea6950b0fddd4c421a1ee46200a3791e4f3e542
Chen Lijun <chenlijun1999@gmail.com>
2020-06-06 10:00:34 +0200

    feat(bst): refactor, fix and test BST

--- src/containers/tree.h
+++ src/containers/tree.h
@@ -8,6 +8,7 @@
 #ifndef UNITNOS_TREE_H_
 #define UNITNOS_TREE_H_
 
+#include "../bool.h"
 #include "base.h"
 
 #include <stdio.h>
@@ -18,7 +19,14 @@ extern "C" {
 #endif
 
 typedef struct unitnos_tree unitnos_tree;
-typedef struct unitnos_node unitnos_node;
+
+/**
+ * Specifies the type of function passed to #unitnos_tree_foreach(). It is
+ * passed the value of each node, together with the optional user_data
+ * parameter passed to #unitnos_tree_foreach(). If the function returns true,
+ * the traversal is stopped.
+ */
+typedef bool (*unitnos_tree_transverse_func)(void *value, void *user_data);
 
 /**
  * Restituisce un nuovo albero
@@ -32,30 +40,53 @@ typedef struct unitnos_node unitnos_node;
 unitnos_tree *unitnos_tree_create(unitnos_compare_func compare_func,
                                   unitnos_destroy_nodify value_destroy_func,
                                   void *user_data);
-
 /**
- * Aggiunge un nodo con il valore indicato
+ * Destroy the tree.
  */
-void unitnos_tree_add_node(unitnos_tree *tree, void *value);
+void unitnos_tree_destroy(unitnos_tree *tree);
+/**
+ * Insert the given value in the tree. If the value is already present, nothing
+ * happens.
+ */
+void unitnos_tree_insert(unitnos_tree *tree, void *value);
+/**
+ * Remove the given value from the tree. If the value is not present, nothing
+ * happens.
+ */
+void unitnos_tree_remove(unitnos_tree *tree, const void *value);
 
 /**
- * Restituisce un nodo col valore indicato
+ * Calls the given function for each of the node in the tree. The function is
+ * passed value of each node, and the given data parameter. The tree is
+ * traversed in sorted order.
  *
- * \retval NULL se non lo trova
+ * The tree may not be modified while iterating over it (you can't add/remove
+ * items). To remove all items matching a predicate, you need to add each item
+ * to a list in your unitnos_tree_transverse_funcFunc as you walk over the
+ * tree, then walk the list and remove each item.
+ *
+ * \param [in] tree a tree
+ * \param [in] func the function to call for each node visited. If this
+ * function returns true, the traversal is stopped.
+ * \param [in] user_data user data to pass to the function
  */
-unitnos_node *unitnos_tree_search(unitnos_tree *tree, void *value);
+void unitnos_tree_foreach(unitnos_tree *tree, unitnos_tree_transverse_func func,
+                          void *user_data);
 
 /**
- * Rimuove il nodo che coincide col valore indicato
+ * Get the number of elements in the tree
  */
-void unitnos_tree_remove(unitnos_tree *tree, void *value);
-
+size_t unitnos_tree_size(unitnos_tree *tree);
 /**
- * Rimuovono ricorsivamente tutti i nodi a partire da un nodo specificato
- * [destroy] o dalla root [destroy_all]
+ * Check whether the given value is present in the tree
  */
-void unitnos_tree_destroy(unitnos_tree *tree, unitnos_node *node);
-void unitnos_tree_destroy_all(unitnos_tree *tree);
+bool unitnos_tree_contains(unitnos_tree *tree, const void *value);
+/**
+ * Check whether a value equivalent to the given value exists in the tree.
+ *
+ * \returns the original value
+ */
+void* unitnos_tree_lookup(unitnos_tree *tree, const void *value);
 
 #ifdef __cplusplus
 }
