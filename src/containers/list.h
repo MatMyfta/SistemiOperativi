6d2615d11d7f6aa583f0f6fd526b0d457c41f0e3
Chen Lijun <chenlijun1999@gmail.com>
2020-06-07 23:53:33 +0200

    refactor: list

--- src/containers/list.h
+++ src/containers/list.h
@@ -7,73 +7,70 @@
 #ifndef UNITNOS_LIST_H_
 #define UNITNOS_LIST_H_
 
+#include "../bool.h"
+#include "base.h"
+
 #include <stddef.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef struct list list;
-typedef struct list_node list_node;
+typedef struct unitnos_list unitnos_list;
+typedef struct unitnos_list_node unitnos_list_node;
+typedef bool (*unitnos_list_transverse_func)(void *value, void *user_data);
+/**
+ * Specifies the type of function passed to #unitnos_list_foreach(). It is
+ * passed the value of each item present in the list, together with the
+ * optional user_data parameter passed to #unitnos_list_foreach(). If the
+ * function returns true, the traversal is stopped.
+ */
 
 /**
- * Use as opaque type
+ * Create a list
  */
-struct list_node {
-  list_node *prev;
-  list_node *next;
-  void *data;
-};
-
-/**
- * Use as opaque type
- */
-struct list {
-  list_node *head;
-  list_node *tail;
-  size_t size;
-  size_t element_size;
-};
-
-list *list_create(size_t element_size);
-void list_destroy(list *list);
+unitnos_list *unitnos_list_create(unitnos_destroy_nodify value_destroy_func,
+                                  void *user_data);
+void unitnos_list_destroy(unitnos_list *list);
 /**
  * Access last element
  */
-void *list_back(list *list);
+void *unitnos_list_back(unitnos_list *list);
 /**
  * Add element at the end
  */
-void list_push_back(list *list, const void *data);
+void unitnos_list_push_back(unitnos_list *list, void *data);
 /**
  * Delete last element
  */
-void list_pop_back(list *list);
+void unitnos_list_pop_back(unitnos_list *list);
 /**
  * Find the node that holds \p data and delete it
  *
  * Complexity: O(n)
  */
-void list_remove(list *list, void *data);
-/**
- * Remove a list node
- *
- * Complexity: O(1)
- */
-void list_remove_node(list *list, list_node *node);
+void unitnos_list_remove(unitnos_list *list, const void *data);
 /**
  * Get the number of items in the list
  *
  * Complexity: O(1)
  */
-size_t list_size(list *list);
+size_t unitnos_list_size(unitnos_list *list);
 
-#define list_for_each_node(it, list)                                           \
-  for (it = list->head; it != NULL; it = it->next)
-
-#define list_for_each_data(_data_, _type_, _it_, _list_)                       \
-  for ((_it_ = _list_->head) && (_data_ = _it_->data); _it_ != NULL;           \
-       (_it_ = _it_->next) && (_data_ = (_type_ *)_it_->data))
+/**
+ * Calls the given function for each of the item in the list. The function is
+ * passed value of each item, and the given user_data parameter.
+ *
+ * The list may not be modified while iterating over it (you can't add/remove
+ * items).
+ *
+ * \param [in] list a list
+ * \param [in] func the function to call for each item visited. If this
+ * function returns true, the traversal is stopped.
+ * \param [in] user_data user data to pass to the function
+ */
+void unitnos_list_foreach(unitnos_list *list, unitnos_list_transverse_func func,
+                          void *user_data);
 
 #ifdef __cplusplus
 }
